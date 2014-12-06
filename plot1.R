98af8acdb8a1bbaa60dacfc632d027d147818a13
RickyPark <rickypark@RickyParkui-MacBook-Pro.local>
2014-12-06 13:26:57 +0900

    plot 3

--- plot1.R
+++ plot1.R
@@ -1,7 +1,9 @@
-unzip('household_power_consumption.zip')
 library('sqldf')
-hpc = read.csv.sql("household_power_consumption.txt", sep = ';', header=TRUE, sql = "select * from file where Date='1/2/2007' or Date='2/2/2007'")
+unzip('household_power_consumption.zip')
+hpc = read.csv.sql("household_power_consumption.txt", sep = ';', header=TRUE, 
+                   sql = "select * from file where Date='1/2/2007' or Date='2/2/2007'")
 png('plot1.png')
-hist(hpc$Global_active_power, col='red', xlab='Global Active Power (kilowatts)', main='Global Active Power')
+hist(hpc$Global_active_power, col='red', xlab='Global Active Power (kilowatts)', 
+     main='Global Active Power')
 dev.off()
 unlink('household_power_consumption.txt')
--- plot3.R
+++ plot3.R
@@ -2,10 +2,14 @@ library('sqldf')
 unzip('household_power_consumption.zip')
 hpc = read.csv.sql("household_power_consumption.txt", sep = ';', header=TRUE, 
                    sql = "select * from file where Date='1/2/2007' or Date='2/2/2007'")
-png('plot2.png')
+png('plot3.png')
 plot(x = strptime(paste(hpc$Date, hpc$Time), format = '%d/%m/%Y %H:%M:%S'), 
-     y=hpc$Global_active_power, type='l', main='Global Active Power', 
-     ylab='Global Active Power', xlab='')
+     y=hpc$Sub_metering_1, type='l', main='', ylab='Energe sub metering', xlab='')
+points(x = strptime(paste(hpc$Date, hpc$Time), format = '%d/%m/%Y %H:%M:%S'), 
+     y=hpc$Sub_metering_2, type='l', col="red")
+points(x = strptime(paste(hpc$Date, hpc$Time), format = '%d/%m/%Y %H:%M:%S'), 
+       y=hpc$Sub_metering_3, type='l', col="blue")
+legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 axis(1, c(as.numeric(strptime('1/2/2007 0:0:0', format='%d/%m/%Y %H:%M:%S')),
           as.numeric(strptime('2/2/2007 0:0:0', format='%d/%m/%Y %H:%M:%S')), 
           as.numeric(strptime('3/2/2007 0:0:0', format='%d/%m/%Y %H:%M:%S'))),
